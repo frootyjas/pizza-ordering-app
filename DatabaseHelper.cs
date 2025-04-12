c3b8e28b55186e1cfbd12661b525dcd0e3147146
jxxdxxexx <169637363+frootyjas@users.noreply.github.com>
2025-04-12 21:16:08 +0800

    add database helper

---
+++ DatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace pizza_ordering_app
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string ConnectionString =
+            "server=sql12.freesqldatabase.com;user id=sql12772758;password=ZfPWDQ2dFW;database=sql12772758;";
+        //private static readonly string ConnectionString = 
+        //    "server=localhost;user id=root;password=;database=pizza_app;";
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public static MySqlCommand CreateCommand(string query, MySqlConnection connection)
+        {
+            return new MySqlCommand(query, connection);
+        }
+    }
+}
